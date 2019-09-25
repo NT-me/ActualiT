6408a86c0db3acbb7217dfd4b8c74a21ab6e0710
gnouf1 <jeuxforum26@gmail.com>
2019-09-25 20:54:05 +0200

    style

--- reddit.py
+++ reddit.py
@@ -1,29 +1,28 @@
 import feedparser as fp
 import json
-import asyncio
-import purifier
 import utils as u
 
 PATH_FileRes = "redditRES.json"
 
+
 def askReddit():
-	#Liste des feeds
-	FILE_liste = open("reddit_list.txt","r")
+	# Liste des feeds
+	FILE_liste = open("reddit_list.txt", "r")
 	liste = u.TxtToList(FILE_liste)
 	res = dict()
 	inc = 0
 
-	#Parcours la liste de source
+	# Parcours la liste de source
 	for item in liste:
-		if item[len(item)-1]=='/':
-			item=item[:len(item)-1]+".rss"
-		else :
+		if item[len(item)-1] == '/':
+			item = item[:len(item)-1]+".rss"
+		else:
 			item = item+".rss"
 		parse = fp.parse(item).entries
-		res[inc]=parse
+		res[inc] = parse
 		# Ajoute la provenance à chaque article
 		for article in res[inc]:
-			article["from"]="reddit"
+			article["from"] = "reddit"
 		inc = inc + 1
 
 	with open(PATH_FileRes, 'w') as f:
