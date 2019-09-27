a2533827077225fafda6c6f105dc947f9d744d21
gnouf1 <jeuxforum26@gmail.com>
2019-09-27 02:47:11 +0200

    new mutli-thread way

--- twitter.py
+++ twitter.py
@@ -7,33 +7,37 @@ PATH_FileRes = "tweetRES.json"
 
 
 class TwitterWork(Thread):
-	def __init__(self, queueIn):
+	def __init__(self, queueIn, queueOut):
 		Thread.__init__(self)
 		self.queueIn = queueIn
+		self.queueOut = queueOut
 
 	def run(self):
-			item = self.queueIn.get()
-			res = ts(item, 40, PATH_FileRes, "json")
-			res.get_profile_tweets()
-			self.queueIn.task_done()
+		item = self.queueIn.get()
+		res = ts(item, 10, PATH_FileRes, "json")
+		self.queueOut.put(res)
+		self.queueIn.task_done()
 
 
-@u.MTime
 def askTwitter():
 	print("--=Start twitter=--")
 	liste = u.TxtToList(open('twitter_list.txt'))
-	#thread_count = 1
-	#queueIn = Queue()
-	for item in liste:
-		res = ts(item, 40, PATH_FileRes, "json")
+	thread_count = len(liste)
+	queueIn = Queue()
+	queueOut = Queue()
+
+	for i in range(thread_count):
+		TW = TwitterWork(queueIn, queueOut)
+		TW.deamon = True
+		TW.start()
+
+	for compte in liste:
+		queueIn.put(compte)
+	queueIn.join()
+
+	while queueOut.empty() == False:
+		res = queueOut.get()
 		res.get_profile_tweets()
-	#for i in range(thread_count):
-	#	TW = TwitterWork(queueIn)
-	#	TW.deamon = True
-	#	TW.start()
-#
-#	for compte in liste:
-#		queueIn.put(compte)
-#
-#	queueIn.join()
+	queueOut.task_done()
+
 	print("--=End twitter=--")
